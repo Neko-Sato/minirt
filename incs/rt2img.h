df2a9ba6c362a07e96923d0f80a2482791e4aa05
Neko-Sato <hisatosi35350@gmail.com>
2024-06-05 03:13:41 +0900

    update

--- incs/rt2img.h
+++ incs/rt2img.h
@@ -6,14 +6,14 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 00:16:18 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/06/05 03:08:38 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/06/05 03:13:20 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RT2IMG_H
 # define RT2IMG_H
 
-#include "objects/scene.h"
+# include "objects/scene.h"
 
 int	rt2img(t_scene *scene, unsigned int *img, int width, int height);
 
