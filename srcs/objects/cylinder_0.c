71f04c2281795b1a4cd087814e4ab560088fb4c9
Neko-Sato <hisatosi35350@gmail.com>
2024-06-19 02:19:09 +0900

    ||a×b||≠1だったわ

--- srcs/objects/cylinder_0.c
+++ srcs/objects/cylinder_0.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 00:58:41 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/06/19 01:52:32 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/06/19 02:17:14 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,8 @@ void	cylinder_update_aabb(t_cylinder *self)
 {
 	t_figure *const	figure = (t_figure *)self;
 	const t_vec3d	u = vec3d_norm(self->axis);
-	const t_vec3d	r = vec3d_cross(
-			(t_vec3d []){{{0, 1, 0}}, {{1, 0, 0}}}[!u._[0] && !u._[2]], u);
+	const t_vec3d	r = vec3d_norm(vec3d_cross(
+				(t_vec3d []){{{0, 1, 0}}, {{1, 0, 0}}}[!u._[0] && !u._[2]], u));
 	const t_vec3d	u1 = vec3d_mul(self->height / 2., u);
 	const t_vec3d	r1 = vec3d_mul(self->diameter / 2., r);
 
