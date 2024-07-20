4b52f97df96406ace91394a32a9cd4ee0c5edf42
Neko-Sato <hisatosi35350@gmail.com>
2024-07-20 21:25:51 +0900

    update

--- srcs/objects/abstract_figure/methods_0.c
+++ srcs/objects/abstract_figure/methods_0.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 13:15:37 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/07/20 04:38:29 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/07/20 04:38:41 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@
 
 t_color	abstract_figure_get_color(t_abstract_figure *self, const t_vec3d *point)
 {
-	(void)point;
+	float	uv[2];
+
+	if (self->_->get_uv_coord && self->checker._.alpha == 0xff)
+	{
+		self->_->get_uv_coord(self, point, uv);
+		if ((int)(uv[0] * 20) % 2 == (int)(uv[1] * 20) % 2)
+			return (self->color);
+		return (self->checker);
+	}
 	return (self->color);
 }
