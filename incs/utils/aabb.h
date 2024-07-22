a76f0e62872f8cda46aa6b6213ecadae5b8cd1fc
Neko-Sato <hisatosi35350@gmail.com>
2024-07-23 01:07:39 +0900

    ローカル座標で計算されたaabbをワールド座標系に戻します

--- incs/utils/aabb.h
+++ incs/utils/aabb.h
@@ -6,15 +6,16 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 10:01:15 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/07/13 10:04:06 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/07/23 00:41:15 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AABB_H
 # define AABB_H
 
-# include "./vec3d.h"
 # include "./ray.h"
+# include "./matrix3x3.h"
+# include "./vec3d.h"
 
 typedef struct s_aabb
 {
@@ -22,7 +23,8 @@ typedef struct s_aabb
 	t_vec3d	max;
 }			t_aabb;
 
-int			aabb_contains(\
-	const t_aabb *aabb, const t_ray *ray, float max_dist);
+int			aabb_contains(const t_aabb *aabb, const t_ray *ray, float max_dist);
+t_aabb		aabb_to_world(const t_aabb *local, const t_matrix3x3 *rotation,
+				const t_vec3d *position);
 
 #endif
--- srcs/utils/aabb.c
+++ srcs/utils/aabb.c
@@ -6,11 +6,12 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 10:04:50 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/07/19 21:42:52 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/07/23 00:41:48 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils/aabb.h"
+#include "utils/matrix3x3.h"
 #include "utils/ray.h"
 #include "utils/vec3d.h"
 #include <math.h>
@@ -34,10 +35,50 @@ int	aabb_contains(const t_aabb *aabb, const t_ray *ray, float max_dist)
 			if (min_dist > max_dist)
 				return (0);
 		}
-		else if (ray->c._[i] < aabb->min._[i]
-			|| ray->c._[i] > aabb->max._[i])
+		else if (ray->c._[i] < aabb->min._[i] || ray->c._[i] > aabb->max._[i])
 			return (0);
 		i++;
 	}
 	return (1);
+}
+
+static inline void	aabb_to_world_update(t_aabb *res, const t_vec3d *tmp)
+{
+	int	i;
+
+	i = 0;
+	while (i < 3)
+	{
+		res->min._[i] = fmin(res->min._[i], tmp->_[i]);
+		res->max._[i] = fmax(res->max._[i], tmp->_[i]);
+		i++;
+	}
+}
+
+t_aabb	aabb_to_world(const t_aabb *local, const t_matrix3x3 *rotation,
+		const t_vec3d *position)
+{
+	t_aabb	res;
+	t_vec3d	tmp;
+	int		i;
+	int		j;
+
+	res = (t_aabb){{{0, 0, 0}}, {{0, 0, 0}}};
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 3)
+		{
+			if (i & (1 << j))
+				tmp._[j] = local->max._[j];
+			else
+				tmp._[j] = local->min._[j];
+			j++;
+		}
+		tmp = vec3d_add(matrix3x3_mul_vec3d(*rotation, tmp), *position);
+		aabb_to_world_update(&res, &tmp);
+		i++;
+	}
+	return (res);
 }
